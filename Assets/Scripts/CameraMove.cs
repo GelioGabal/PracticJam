c7a336d4d7117d504f008e5d8ca717db71797b99
Stephan0404 <stephan906082@gmail.com>
2024-08-04 17:51:30 +0300

    Camera movement

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInput playerInput;
+    InputAction Moveaction; 
+    static public Vector3 face;
+    [SerializeField] float speed = 1;
+
+    private void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        Moveaction = playerInput.actions.FindAction("Movement");
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Update()
     {
-        
+        Move();
     }
+
+
+
+    private void Move()
+    {
+        Vector2 direction = Moveaction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x,direction.y,0)*Time.deltaTime*speed;
+    }
+
 }
